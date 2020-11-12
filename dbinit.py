e7549f0424197b88091f6304443db8486bda9b30
Onurcan Camci <onurcamci55@gmail.com>
2020-11-12 18:40:09 +0300

    feat: migrations

--- dbinit.py
+++ dbinit.py
@@ -5,16 +5,40 @@ import psycopg2 as dbapi2
 
 
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+    "CREATE TABLE IF NOT EXISTS migration_version (version INTEGER)",
 ]
 
+MIGRATIONS = [
+    [
+        "CREATE TABLE test (asd INTEGER)"
+    ],
+    [
+        "DROP TABLE test"
+    ]
+]
+
+
+def current_version(cursor):
+    cursor.execute("SELECT version FROM migration_version")
+    result = cursor.fetchall()
+    if len(result) == 0:
+        cursor.execute("INSERT INTO migration_version VALUES (0)")
+        return 0
+    return result[0][0]
+
+def inc_version(cursor):
+    cursor.execute("UPDATE migration_version SET version = version + 1")
 
 def initialize(url):
     with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
         for statement in INIT_STATEMENTS:
             cursor.execute(statement)
+        version = current_version(cursor)
+        for k in range(version, len(MIGRATIONS)):
+            for q in MIGRATIONS[k]:
+                cursor.execute(q)
+            inc_version(cursor)
         cursor.close()
 
 
