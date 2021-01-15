07b1ac630d916679306919b5793d917ed232d6c8
Onurcan Camci <onurcamci55@gmail.com>
2021-01-16 01:19:12 +0300

    fix: init sql

--- src/migrations/initial_db.sql
+++ src/migrations/initial_db.sql
@@ -1,29 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 12.3 (Debian 12.3-1.pgdg100+1)
--- Dumped by pg_dump version 12.2
-
---SET statement_timeout = 0;
---SET lock_timeout = 0;
---SET idle_in_transaction_session_timeout = 0;
---SET client_encoding = 'UTF8';
---SET standard_conforming_strings = on;
---SELECT pg_catalog.set_config('search_path', '', false);
---SET check_function_bodies = false;
---SET xmloption = content;
---SET client_min_messages = warning;
---SET row_security = off;
-
---SET default_tablespace = '';
-
---SET default_table_access_method = heap;
-
---
--- Name: chunks; Type: TABLE; Schema: public; Owner: postgres
---
-
 CREATE TABLE chunks (
     competition_id integer NOT NULL,
     x integer NOT NULL,
@@ -32,9 +6,6 @@ CREATE TABLE chunks (
 );
 
 
---
--- Name: competitions; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.competitions (
     id integer NOT NULL,
@@ -45,11 +16,6 @@ CREATE TABLE public.competitions (
 );
 
 
-
---
--- Name: competitions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
 CREATE SEQUENCE public.competitions_id_seq
     AS integer
     START WITH 1
@@ -59,17 +25,7 @@ CREATE SEQUENCE public.competitions_id_seq
     CACHE 1;
 
 
-
---
--- Name: competitions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.competitions_id_seq OWNED BY public.competitions.id;
-
-
---
--- Name: event_log; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.event_log (
     id integer NOT NULL,
@@ -77,12 +33,6 @@ CREATE TABLE public.event_log (
     competition_id integer,
     action text
 );
-
-
-
---
--- Name: event_log_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.event_log_id_seq
     AS integer
@@ -92,18 +42,7 @@ CREATE SEQUENCE public.event_log_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
-
---
--- Name: event_log_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.event_log_id_seq OWNED BY public.event_log.id;
-
-
---
--- Name: flags; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.flags (
     competition_id integer NOT NULL,
@@ -115,12 +54,6 @@ CREATE TABLE public.flags (
     state integer NOT NULL
 );
 
-
-
---
--- Name: guesses; Type: TABLE; Schema: public; Owner: postgres
---
-
 CREATE TABLE public.guesses (
     competition_id integer NOT NULL,
     chunk_x integer NOT NULL,
@@ -130,22 +63,10 @@ CREATE TABLE public.guesses (
     user_id integer NOT NULL
 );
 
-
-
---
--- Name: sessions; Type: TABLE; Schema: public; Owner: postgres
---
-
 CREATE TABLE public.sessions (
     session_id character varying(255)[] NOT NULL,
     user_id integer NOT NULL
 );
-
-
-
---
--- Name: user_entries; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.user_entries (
     user_id integer NOT NULL,
@@ -155,25 +76,12 @@ CREATE TABLE public.user_entries (
     mines_hit integer DEFAULT 0 NOT NULL
 );
 
-
-ALTER TABLE public.user_entries OWNER TO postgres;
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
-
 CREATE TABLE public.users (
     id integer NOT NULL,
     username character varying(100)[] NOT NULL,
     password character varying(255)[] NOT NULL,
     elo integer DEFAULT 1000 NOT NULL
 );
-
-
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.users_id_seq
     START WITH 1
@@ -182,181 +90,65 @@ CREATE SEQUENCE public.users_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- Name: competitions id; Type: DEFAULT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.competitions ALTER COLUMN id SET DEFAULT nextval('public.competitions_id_seq'::regclass);
 
-
---
--- Name: event_log id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.event_log ALTER COLUMN id SET DEFAULT nextval('public.event_log_id_seq'::regclass);
 
-
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- Name: chunks chunks_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.chunks
     ADD CONSTRAINT chunks_pkey PRIMARY KEY (competition_id, x, y);
 
-
---
--- Name: competitions competitions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.competitions
     ADD CONSTRAINT competitions_pkey PRIMARY KEY (id);
-
-
---
--- Name: event_log event_log_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.event_log
     ADD CONSTRAINT event_log_pkey PRIMARY KEY (id);
 
-
---
--- Name: flags flags_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.flags
     ADD CONSTRAINT flags_pkey PRIMARY KEY (competition_id, chunk_x, chunk_y, offset_x, offset_y);
-
-
---
--- Name: guesses guesses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.guesses
     ADD CONSTRAINT guesses_pkey PRIMARY KEY (competition_id, chunk_x, chunk_y, offset_x, offset_y);
 
-
---
--- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.sessions
     ADD CONSTRAINT sessions_pkey PRIMARY KEY (session_id);
-
-
---
--- Name: user_entries user_entries_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.user_entries
     ADD CONSTRAINT user_entries_pkey PRIMARY KEY (user_id, competition_id);
 
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
-
---
--- Name: user_entries competitionid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.user_entries
     ADD CONSTRAINT competitionid FOREIGN KEY (competition_id) REFERENCES public.competitions(id);
-
-
---
--- Name: chunks competitionid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.chunks
     ADD CONSTRAINT competitionid FOREIGN KEY (competition_id) REFERENCES public.competitions(id);
 
-
---
--- Name: event_log competitionid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.event_log
     ADD CONSTRAINT competitionid FOREIGN KEY (competition_id) REFERENCES public.competitions(id);
-
-
---
--- Name: flags competitionid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.flags
     ADD CONSTRAINT competitionid FOREIGN KEY (competition_id) REFERENCES public.competitions(id);
 
-
---
--- Name: guesses competitionid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.guesses
     ADD CONSTRAINT competitionid FOREIGN KEY (competition_id) REFERENCES public.competitions(id);
-
-
---
--- Name: sessions userid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.sessions
     ADD CONSTRAINT userid FOREIGN KEY (user_id) REFERENCES public.users(id) NOT VALID;
 
-
---
--- Name: user_entries userid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.user_entries
     ADD CONSTRAINT userid FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: event_log userid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.event_log
     ADD CONSTRAINT userid FOREIGN KEY (user_id) REFERENCES public.users(id);
 
-
---
--- Name: flags userid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.flags
     ADD CONSTRAINT userid FOREIGN KEY (user_id) REFERENCES public.users(id);
 
-
---
--- Name: guesses userid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.guesses
     ADD CONSTRAINT userid FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- PostgreSQL database dump complete
---
 
